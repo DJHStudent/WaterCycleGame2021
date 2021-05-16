011839db53310692665a9ce90903d34181c1229a
rabbitsfoot33 <82695353+rabbitsfoot33@users.noreply.github.com>
2021-05-17 00:49:29 +1000

    implemented bug obstacle lvl2

--- Assets/Scripts/MoveSide.cs
+++ Assets/Scripts/MoveSide.cs
@@ -4,20 +4,59 @@ using UnityEngine;
 
 public class MoveSide : MonoBehaviour
 {
-    protected float speed = 20;
+    bool direction; 
+    bool randomBoolean()
+{
+    if (Random.value >= 0.5)
+    {
+        return true;
+    }
+    return false;
+}
+    protected float speed = 5;
     protected float destroyXPos = -54;
 
     // Update is called once per frame
+
+    void start()
+    {
+        Vector2 pos = transform.position;
+        if(direction == true)
+        {
+            pos.x = 40;
+        }
+        else
+        {
+            pos.x = -40;
+        }
+        transform.position = pos;
+        direction = randomBoolean();
+
+ 
+        
+
+        
+    }
     protected virtual void Update()
     {
         if (whenPause()) //move at the specified speed until reach level bottom
         {
-            transform.Translate(Vector2.right * speed * GameManager.levelStats.speed * Time.deltaTime, Space.World);
+            if (direction == true)
+        {
+          transform.Translate(Vector2.right * speed * GameManager.levelStats.speed * Time.deltaTime, Space.World); 
+        }
+        else
+        {
+           transform.Translate(Vector2.left * speed * GameManager.levelStats.speed * Time.deltaTime, Space.World); 
+        }
+
             if (whenDestroy())
             {
                 Destroy(this.gameObject);
             }
         }
+
+        
     }
 
     protected virtual bool whenDestroy()
@@ -29,4 +68,6 @@ public class MoveSide : MonoBehaviour
     {
         return !GameManager.levelStats.paused;
     }
+
+    
 }
